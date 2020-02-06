3743cb744d6f386798f110d1763b8bba0021d94a
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-02-06 23:55:58 +0000

    Add components

--- vhdl-processor.srcs/sources_1/new/register_file.vhd
+++ vhdl-processor.srcs/sources_1/new/register_file.vhd
@@ -48,7 +48,36 @@ entity register_file is
 end register_file;
 
 architecture Behavioral of register_file is
+   component decoder_3to8 is
+      port(A : in std_logic_vector(2 downto 0);
+           Q : out std_logic_vector(7 downto 0));
+   end component;
 
+   component multiplexer2_16bit is
+      port(s : in std_logic;
+           in1 : in std_logic_vector(15 downto 0);
+           in2 : in std_logic_vector(15 downto 0);
+           z : out std_logic_vector(15 downto 0));
+   end component;
+
+   component reg16 is
+      port(D : in std_logic_vector(15 downto 0);
+      load, Clk : in std_logic;
+      Q : out std_logic_vector(15 downto 0));
+   end component;
+
+   component multiplexer8_16bit is
+      port(s : in std_logic_vector(2 downto 0);
+           in1 : in std_logic_vector(15 downto 0);
+           in2 : in std_logic_vector(15 downto 0);
+           in3 : in std_logic_vector(15 downto 0);
+           in4 : in std_logic_vector(15 downto 0);
+           in5 : in std_logic_vector(15 downto 0);
+           in6 : in std_logic_vector(15 downto 0);
+           in7 : in std_logic_vector(15 downto 0);
+           in8 : in std_logic_vector(15 downto 0);
+           z : out std_logic_vector(15 downto 0));
+   end component;
 begin
 
 
