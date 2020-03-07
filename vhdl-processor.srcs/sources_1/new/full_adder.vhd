ecee366a00b55fe6043e3996bdedc36494ee3808
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-03-07 23:16:31 +0000

    Add gate delay

--- vhdl-processor.srcs/sources_1/new/full_adder.vhd
+++ vhdl-processor.srcs/sources_1/new/full_adder.vhd
@@ -32,20 +32,20 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity full_adder is
-   port(in1 : in std_logic;
-	in2 : in std_logic;
-	c_in : in std_logic;
-	c_out : out std_logic;
-	sum : out std_logic);
+  port(in1 : in std_logic;
+       in2 : in std_logic;
+       c_in : in std_logic;
+       c_out : out std_logic;
+       sum : out std_logic);
 end full_adder;
 
 architecture Behavioral of full_adder is
   constant gate_delay: Time := 1 ns;
   signal s1,s2,s3 : std_logic;
 begin
-   s1 <= in1 xor in2;
-   s2 <= s1 and c_in;
-   s3 <= in1 and in2;
-   sum <= s1 xor c_in;
-   c_out <= s2 or s3;
+  s1 <= in1 xor in2 after gate_delay;
+  s2 <= s1 and c_in after gate_delay;
+  s3 <= in1 and in2 after gate_delay;
+  sum <= s1 xor c_in after gate_delay;
+  c_out <= s2 or s3 after gate_delay;
 end Behavioral;
