89fda08481db0af3149f201397a335efb9ea706c
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-04-13 01:22:59 +0100

    Add support for branching instructions

--- assembler/assemble.py
+++ assembler/assemble.py
@@ -21,21 +21,27 @@ def translate(assembly):
     tokens = assembly.split()
     machine_code = 0
     n = len(tokens)
+    opcode = OPCODE_TABLE[tokens[0].lower()]
+    nums = re.findall(r'-?\d+', assembly)
+    nums = list(map(int, nums))
     if n == 4:
-        nums = re.findall(r'\d+', assembly)
-        nums = map(int, nums)
-        opcode = OPCODE_TABLE[tokens[0].lower()]
         dst = nums[0]
         src1 = nums[1]
         src2 = nums[2]
         machine_code = opcode << 9 | dst << 6 | src1 << 3 | src2
+    elif n == 2:
+        mask = 0b111
+        displacement = nums[0]
+        displacement_left = displacement & (mask << 3)
+        displacement_right = displacement & mask
+        machine_code = opcode << 9 | displacement_left << 3 | displacement_right
     return machine_code
 
 
 def main():
     for line in sys.stdin.readlines():
         machine_code = translate(line)
-        formatted = 'x\"{:04x}\"'.format(machine_code)
+        formatted = 'x\"{0:04x}\" {0:b}'.format(machine_code)
         print(formatted)
 
 
